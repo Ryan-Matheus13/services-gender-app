1275ff9b008c68d5e5c27e268bd1b67e695699b4
Ryan Matheus <ryanmatheusdev@gmail.com>
2024-05-21 03:07:51 -0300

    feat: definindo telas

---
+++ src/pages/App/Scheduling.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Scheduling() {
+  return (
+    <View>
+      <Text>Scheduling</Text>
+    </View>
+  );
+}
---
+++ src/pages/App/SchedulingDoctors.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function SchedulingDoctors() {
+  return (
+    <View>
+      <Text>SchedulingDoctors</Text>
+    </View>
+  );
+}
